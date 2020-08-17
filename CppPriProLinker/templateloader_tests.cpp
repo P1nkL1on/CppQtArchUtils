b7425f50cafd91ae617ae3b5f11fbd111e324957
Leonid <lion6149@mail.ru>
2020-08-17 18:24:52 +0300

    Fixed template loader tests

--- CppPriProLinker/templateloader_tests.cpp
+++ CppPriProLinker/templateloader_tests.cpp
@@ -18,21 +18,42 @@ void TemplateLoaderTests::readEmptyFile()
     FileData data;
     QString errMsg;
     QVERIFY(TemplateLoader::readFileData(":/empty.txt", "\r\n", data, errMsg));
-    QCOMPARE(data, QStringList());
     QCOMPARE(errMsg, QString());
+    QCOMPARE(data, QStringList());
 }
 
 void TemplateLoaderTests::readMultiLine()
 {
-
+    FileData data;
+    QString errMsg;
+    QVERIFY(TemplateLoader::readFileData(":/multiline.txt", "\r\n", data, errMsg));
+    QCOMPARE(errMsg, QString());
+    const QStringList expectedData = {
+        "usuall line",
+        "\ttabbed line",
+        "",
+        "^ empty line",
+        "\t\t\t\t\t",
+        "^ tabbled empty line",
+        "\tv also empty",
+        ""
+    };
+    QCOMPARE(data, expectedData);
 }
 
 void TemplateLoaderTests::readOneLine()
 {
-
+    FileData data;
+    QString errMsg;
+    QVERIFY(TemplateLoader::readFileData(":/oneline.txt", "\r\n", data, errMsg));
+    QCOMPARE(errMsg, QString());
+    QCOMPARE(data, QStringList{"\tany one line\t  \t"});
 }
 
 void TemplateLoaderTests::readUnexist()
 {
-
+    FileData data;
+    QString errMsg;
+    QVERIFY(not TemplateLoader::readFileData(":/none.txt", "\r\n", data, errMsg));
+    QVERIFY(not errMsg.isEmpty());
 }
